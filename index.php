79f295cee22978a687c9646b3c98655e5fd6108b
Dooruss <doorusdebeijer@gmail.com>
2024-11-22 12:51:36 +0100

    e

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $getReactions = Reactions::getReactions();
 //uncomment de volgende regel om te kijken hoe de array van je reactions eruit ziet
 // echo "<pre>".var_dump($getReactions)."</pre>";
 
-if(!empty($_POST)){
+if (!empty($_POST)) {
 
     //dit is een voorbeeld array.  Deze waardes moeten erin staan.
     $postArray = [
@@ -17,27 +17,41 @@ if(!empty($_POST)){
 
     $setReaction = Reactions::setReaction($postArray);
 
-    if(isset($setReaction['error']) && $setReaction['error'] != ''){
+    if (isset($setReaction['error']) && $setReaction['error'] != '') {
         prettyDump($setReaction['error']);
     }
-    
-
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Youtube remake</title>
 </head>
+
 <body>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/_BHkndCP2Rg?si=aMbvYZ6NV5Zh42tI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
     <h2>reactions</h2>
-    <p>Maak hier je eigen pagina van aan de hand van de opdracht</p>
+
+    <form action="" method="post">
+        <input type="text" placeholder="Name here" name="naam">
+        <br>
+        <textarea id="review" placeholder="Comment here" name="message"></textarea>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+    <h2> the reactions here <h2>
+
+            <?php
+            echo "<pre>" . var_dump($getReactions) . "</pre>";
+            echo ("<h2>" . count($getReactions) . "</h2>");
+            ?>
+
 </body>
+
 </html>
 
 <?php
